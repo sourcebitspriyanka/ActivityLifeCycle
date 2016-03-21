10dab36543a4a27973a4eccb32fd69cbe8e2ab35
sourcebitspriyanka <priyankat@sourcebitss-iMac-6.local>
2016-03-21 14:53:14 +0530

    commented button
    error occurs

--- app/src/main/java/com/example/priyankatotakanuma/activitylifecycle/MainActivity.java
+++ app/src/main/java/com/example/priyankatotakanuma/activitylifecycle/MainActivity.java
@@ -8,7 +8,7 @@ import android.view.View;
 import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-    private Button button;
+   // private Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
